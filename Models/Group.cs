95593534b180aaaac94d760ffc8b835a6c088915
kamra <kamra@LAPTOP-M49P7DNF>
2022-05-13 21:14:08 +0400

    First Commit (Classes and Enum added)

---
+++ Models/Group.cs
@@ -0,0 +1,64 @@
+﻿using Console_Application.Enum;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Console_Application.Models
+{
+    public class Group
+    {
+        public string No;
+        public Category Category;
+        public bool IsOnline;
+        byte _limit;
+        List<Student> students = new List<Student>();
+        public static int count = 100;
+
+        public Group(string no,Category category)
+        {
+            switch (category)
+            {
+                case Category.Programming:
+                    No = $"P" + count;
+                    break;
+                case Category.Design:
+                    No = $"D" + count;
+                    break;
+                case Category.SytemAdministration:
+                    No = $"SA" + count;
+                    break;
+                default:
+                    break;
+            }
+            Category = category;
+        }
+
+        public byte Limit { 
+            get 
+            {
+                return _limit;
+            } 
+            set
+            {
+                if (!IsOnline)
+                {
+                    _limit = 15;
+                    Console.WriteLine("Bu qrup online ");
+                }
+                else if (IsOnline)
+                {
+                    _limit = 10;
+                    Console.WriteLine("Bu offline qrup-dur");
+                }
+               
+            } 
+        }
+        public void AddStudent(Student student) 
+        {
+            students.Add(student);
+        }
+
+
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Console_Application.Models
+{
+    public class Student
+    {
+        int _id;
+        string _fullName;
+        public byte GroupNumber;
+        public bool Type;
+
+        public Student(int id,string fullName,byte groupNumber,bool type)
+        {
+            _id = id;
+            FullName = fullName;
+            GroupNumber = groupNumber;
+            Type = type;
+        }
+
+        public int Id 
+        { get 
+            {
+                return _id;
+            } 
+           
+        }
+
+        public string FullName 
+        {   get 
+            {
+                return _fullName;
+            } 
+            set 
+            {
+                if (value.Contains(' '))
+                {
+                    FullName = value;
+                }
+                else
+                {
+                    Console.WriteLine("Enter correct Name and Surname");
+                }
+            }
+        }
+    }
+}
